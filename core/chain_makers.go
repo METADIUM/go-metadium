ethereum/go-ethereum

eth/downloader: concurrent receipt and state processing

core/chain_makers.go
@@ -227,7 +227,7 @@ func newCanonical(n int, full bool) (ethdb.Database, *BlockProcessor, error) {
 	}
 	// Header-only chain requested
 	headers := makeHeaderChain(genesis.Header(), n, db, canonicalSeed)
-	_, err := chainman.InsertHeaderChain(headers, true)
+	_, err := chainman.InsertHeaderChain(headers, 1)
 	return db, processor, err
 }
 

METADIUM/go-metadium

core/chain_makers.go
@@ -239,7 +239,7 @@ func newCanonical(n int, full bool) (ethdb.Database, *BlockProcessor, error) {
 	}
 	// Header-only chain requested
 	headers := makeHeaderChain(genesis.Header(), n, db, canonicalSeed)
-	_, err := blockchain.InsertHeaderChain(headers, true)
+	_, err := blockchain.InsertHeaderChain(headers, 1)
 	return db, processor, err
 }
 