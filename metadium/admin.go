wemixarchive/go-wemix

consensus/ethash,miner: Fixed issue with coinbase setup.

wemix/admin.go
@@ -1204,6 +1204,36 @@ func verifyRewards(num *big.Int, rewards string) error {
 	//return admin.verifyRewards(num, rewards)
 }
 
+func getCoinbase(height *big.Int) (coinbase common.Address, err error) {
+	if admin == nil {
+		err = wemixminer.ErrNotInitialized
+		return
+	}
+	prvKey := admin.stack.Server().PrivateKey
+	if admin.self != nil {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		num := new(big.Int).Sub(height, common.Big1)
+		_, gov, _, _, err2 := admin.getRegGovEnvContracts(ctx, num)
+		if err2 != nil {
+			err = err2
+			return
+		}
+
+		nodeId := crypto.FromECDSAPub(&prvKey.PublicKey)[1:]
+		if addr, err2 := enodeExists(ctx, height, gov, nodeId); err2 != nil {
+			err = err2
+			return
+		} else {
+			coinbase = addr
+		}
+	} else if admin.nodeInfo != nil && admin.nodeInfo.ID == admin.bootNodeId {
+		coinbase = admin.bootAccount
+	}
+	return
+}
+
 func signBlock(height *big.Int, hash common.Hash) (coinbase common.Address, sig []byte, err error) {
 	if admin == nil {
 		err = wemixminer.ErrNotInitialized
@@ -1787,6 +1817,7 @@ func init() {
 	wemixminer.SuggestGasPriceFunc = suggestGasPrice
 	wemixminer.CalculateRewardsFunc = calculateRewards
 	wemixminer.VerifyRewardsFunc = verifyRewards
+	wemixminer.GetCoinbaseFunc = getCoinbase
 	wemixminer.SignBlockFunc = signBlock
 	wemixminer.VerifyBlockSigFunc = verifyBlockSig
 	wemixminer.RequirePendingTxsFunc = requirePendingTxs

miner/worker.go
@@ -1632,6 +1632,11 @@ func (w *worker) commitWork(interrupt *int32, noempty bool, timestamp int64) {
 	}
 
 	if !wemixminer.IsPoW() { // Wemix
+		parent := w.chain.CurrentBlock()
+		getCoinbase, err := wemixminer.GetCoinbase(parent.Number())
+		if err == nil {
+			work.coinbase = getCoinbase
+		}
 		if !w.commitTransactionsEx(work, interrupt, start) {
 			w.commitEx(work, w.fullTaskHook, true, start)
 		}

METADIUM/go-metadium

metadium/admin.go
@@ -1232,6 +1232,36 @@ func verifyRewards(num *big.Int, rewards string) error {
 	//return admin.verifyRewards(num, rewards)
 }
 
+func getCoinbase(height *big.Int) (coinbase common.Address, err error) {
+	if admin == nil {
+		err = metaminer.ErrNotInitialized
+		return
+	}
+	prvKey := admin.stack.Server().PrivateKey
+	if admin.self != nil {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		num := new(big.Int).Sub(height, common.Big1)
+		_, gov, _, _, err2 := admin.getRegGovEnvContracts(ctx, num)
+		if err2 != nil {
+			err = err2
+			return
+		}
+
+		nodeId := crypto.FromECDSAPub(&prvKey.PublicKey)[1:]
+		if addr, err2 := enodeExists(ctx, height, gov, nodeId); err2 != nil {
+			err = err2
+			return
+		} else {
+			coinbase = addr
+		}
+	} else if admin.nodeInfo != nil && admin.nodeInfo.ID == admin.bootNodeId {
+		coinbase = admin.bootAccount
+	}
+	return
+}
+
 func signBlock(height *big.Int, hash common.Hash, isPangyo bool) (coinbase common.Address, nodeId, sig []byte, err error) {
 	if admin == nil {
 		err = metaminer.ErrNotInitialized
@@ -1865,6 +1895,7 @@ func init() {
 	metaminer.SuggestGasPriceFunc = suggestGasPrice
 	metaminer.CalculateRewardsFunc = calculateRewards
 	metaminer.VerifyRewardsFunc = verifyRewards
+	metaminer.GetCoinbaseFunc = getCoinbase
 	metaminer.SignBlockFunc = signBlock
 	metaminer.VerifyBlockSigFunc = verifyBlockSig
 	metaminer.RequirePendingTxsFunc = requirePendingTxs

miner/worker.go
@@ -1646,6 +1646,11 @@ func (w *worker) commitWork(interrupt *int32, noempty bool, timestamp int64) {
 	}
 
 	if !metaminer.IsPoW() { // Metadium
+		parent := w.chain.CurrentBlock()
+		getCoinbase, err := metaminer.GetCoinbase(parent.Number())
+		if err == nil {
+			work.coinbase = getCoinbase
+		}
 		if !w.commitTransactionsEx(work, interrupt, start) {
 			w.commitEx(work, w.fullTaskHook, true, start)
 		}